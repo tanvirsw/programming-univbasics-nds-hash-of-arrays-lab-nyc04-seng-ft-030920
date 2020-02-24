eecc65d48eaa6ac107d305c14bbf455d1f0a486e
Tanvir Walia <tarywalia@gmail.com>
2020-02-24 17:58:41 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -11,6 +11,7 @@ def add_character(show, name)
     :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
     :jetsons => ["George", "Jane", "Judy", "Elroy"]
   }
+  show
   # Write your implementation here
   # Should return the array of the 'show' argument
 end
